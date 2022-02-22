b9a50d8be3a295062cafd4e8f9a7e0bb73b14b3c
Lucas de Hoz <dehozlucas@gmail.com>
2022-02-22 19:53:41 -0300

    ADDED ACTOPMES

--- Assets/Scripts/Villager.cs
+++ Assets/Scripts/Villager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
@@ -12,29 +13,227 @@ public enum Roles
 public class Villager : MonoBehaviour
 {
     public Roles rol;
-    [SerializeField]
-    private NavMeshAgent agent;
+    public NavMeshAgent agent;
+
+    int currentActivity;
+    private ActionTasks baseTasks;
+
+    Stack<ActionTasks> toDoTasks = new Stack<ActionTasks>();
+
+    bool doingDailies;
+
+    private List<Actions> newTasks;
+
+    Vector3 startPos;
 
     private void Start()
     {
-        Market[] allMarkets = FindObjectsOfType<Market>();
-        if (this.rol == Roles.Villager)
+        startPos = transform.position;
+        var allMarkets = FindObjectsOfType<Market>();
+        baseTasks = new ActionTasks();
+
+        if (rol == Roles.Villager)
         {
-            Transform toGo = allMarkets.OrderBy(x => (x.transform.position - this.transform.position).magnitude).FirstOrDefault().buyerPosition;
-            this.agent.SetDestination(toGo.position);
+            var toGo = allMarkets.OrderBy(x => (x.transform.position - transform.position).magnitude).FirstOrDefault().buyerPosition;
+            baseTasks.AddAction(new GoToAction(this, toGo.position));
+            baseTasks.AddAction(new WaitAction(this, 5));
+            baseTasks.AddAction(new GoToAction(this, startPos));
         }
         else if (this.rol == Roles.Priest)
         {
         }
         else if (this.rol == Roles.Trader)
         {
-            Transform toGo = allMarkets.OrderBy(x => (x.transform.position - this.transform.position).magnitude).FirstOrDefault().traderPosition;
-            this.agent.SetDestination(toGo.position);
+            var toGo = allMarkets.OrderBy(x => (x.transform.position - transform.position).magnitude).FirstOrDefault().traderPosition;
+            baseTasks.AddAction(new GoToAction(this, toGo.position));
+            baseTasks.AddAction(new WaitAction(this, 5));
+            baseTasks.AddAction(new GoToAction(this, startPos));
         }
+
+        toDoTasks.Push(baseTasks);
+        baseTasks.StartActivities();
     }
 
-    public void OnDieHandler()
+    public void OverrideTasks(ActionTasks newTasks)
     {
-        Destroy(this.agent);
+        toDoTasks.Peek().Stop();
+        toDoTasks.Push(newTasks);
+        toDoTasks.Peek().StartActivities();
+        newTasks.OnEndAllTasks += ResumeBase;
+    }
+
+    public void ResumeBase()
+    {
+        toDoTasks.Pop().OnEndAllTasks -= ResumeBase;
+        toDoTasks.Peek().Resume();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            var asd = new ActionTasks();
+            asd.AddAction(new WaitAction(this, 2));
+            OverrideTasks(asd);
+        }
+
+        toDoTasks.Peek().Update();
     }
 }
+
+public class ActionTasks
+{
+    public System.Action OnEndAllTasks;
+
+    private List<Actions> actions = new List<Actions>();
+
+    private int currentAction;
+
+    public void AddAction(Actions action)
+    {
+        actions.Add(action);
+    }
+
+    public void Stop()
+    {
+        actions[currentAction].Stop();
+    }
+
+    public void Resume()
+    {
+        actions[currentAction].Execute();
+    }
+
+    public void StartActivities(Actions action)
+    {
+        action.OnComplete += OnActionComplete;
+        action.Execute();
+    }
+
+    public void StartActivities()
+    {
+        currentAction = 0;
+        actions[currentAction].OnComplete += OnActionComplete;
+        actions[currentAction].Execute();
+    }
+
+    public void Update()
+    {
+        actions[currentAction].Update();
+    }
+
+    private void OnActionComplete()
+    {
+        actions[currentAction].OnComplete -= OnActionComplete;
+
+        currentAction++;
+        if (currentAction >= actions.Count)
+        {
+            currentAction = 0;
+            OnEndAllTasks?.Invoke();
+        }
+        else
+        {
+            StartActivities(actions[currentAction]);
+        }
+    }
+}
+
+public abstract class Actions
+{
+    public Villager vill;
+    public System.Action OnComplete;
+    public abstract void Update();
+    public abstract void Stop();
+    public abstract void Execute();
+}
+
+public class GoToAction : Actions
+{
+    Vector3 toPosition;
+
+    public GoToAction(Villager vill, Vector3 toPosition)
+    {
+        this.vill = vill;
+        this.toPosition = toPosition;
+    }
+
+    public override void Execute()
+    {
+        vill.agent.isStopped = false;
+        vill.agent.SetDestination(toPosition);
+    }
+
+    public override void Stop()
+    {
+        vill.agent.isStopped = true;
+    }
+
+    public override void Update()
+    {
+        if(Vector3.SqrMagnitude(vill.transform.position - toPosition) <= .5f)
+        {
+            OnComplete?.Invoke();
+        }
+    }
+}
+
+public class WaitAction : Actions
+{
+    private float targetTime;
+
+    public WaitAction(Villager vill, float time)
+    {
+        this.vill = vill;
+        targetTime = time;
+    }
+
+    public override void Execute()
+    {
+
+    }
+
+    public override void Stop()
+    {
+    }
+
+    public override void Update()
+    {
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0)
+        {
+            OnComplete?.Invoke();
+        }
+    }
+}
+
+public class WanderAction : Actions
+{
+    private float wanderRadius;
+    private float wanderTime;
+    private float waitTime;
+
+    public WanderAction(Villager vill, float wanderRadius, float wanderTime, float waitTime)
+    {
+        this.vill = vill;
+        this.wanderRadius = wanderRadius;
+        this.wanderTime = wanderTime;
+        this.waitTime = waitTime;
+    }
+
+    public override void Execute()
+    {
+        vill.agent.isStopped = false;
+        vill.agent.SetDestination(vill.transform.position + Vector3.ProjectOnPlane((Random.insideUnitSphere * wanderRadius), Vector3.up));
+    }
+    public override void Stop()
+    {
+        vill.agent.isStopped = true;
+    }
+
+    public override void Update()
+    {
+         OnComplete?.Invoke();
+    }
+}
+
