ef3c81670333f972591038b5a6aeb4819f2777d3
Lucas de Hoz <dehozlucas@gmail.com>
2022-02-23 20:35:11 -0300

    Implemented role manager

--- Assets/Scripts/RoleManager.cs
+++ Assets/Scripts/RoleManager.cs
@@ -22,30 +22,43 @@ public class RoleManager : MonoBehaviour
 
     private void SetupRoles()
     {
-        foreach (var vill in allVillagers)
+        float current = 0;
+
+        float tradersCount = (float)allMarkets.Length;
+        float WandererCount = (float)allVillagers.Length * 0.1f;
+        float BuyerCount = (float)allVillagers.Length * 0.3f;
+
+        for (int i = 0; i < allVillagers.Length; i++)
         {
+            Villager vill = allVillagers[i];
             ActionTasks baseTasks = new ActionTasks();
 
-            if (vill.rol == Roles.Villager)
+            if (i < tradersCount)
+            {
+                var toGo = allMarkets[i].traderPosition;
+                baseTasks.AddAction(new GoToAction(vill, toGo.position));
+                baseTasks.AddAction(new WaitAction(vill, 5));
+                baseTasks.AddAction(new GoToAction(vill, vill.startPos));
+            }
+            else
             {
                 var toGo = allMarkets.OrderBy(x => (x.transform.position - vill.transform.position).magnitude).FirstOrDefault().buyerPosition;
                 baseTasks.AddAction(new GoToAction(vill, toGo.position));
                 baseTasks.AddAction(new WaitAction(vill, 5));
                 baseTasks.AddAction(new GoToAction(vill, vill.startPos));
             }
-            else if (vill.rol == Roles.Priest)
-            {
-            }
-            else if (vill.rol == Roles.Trader)
-            {
-                var toGo = allMarkets.OrderBy(x => (x.transform.position - vill.transform.position).magnitude).FirstOrDefault().traderPosition;
-                baseTasks.AddAction(new GoToAction(vill, toGo.position));
-                baseTasks.AddAction(new WaitAction(vill, 5));
-                baseTasks.AddAction(new GoToAction(vill, vill.startPos));
-            }
-
 
             vill.Setup(baseTasks);
+            current++;
         }
+    }
+
+    private ActionTasks WanderTasks(Villager vill)
+    {
+        ActionTasks baseTasks = new ActionTasks();
+
+        baseTasks.AddAction(new WanderAction(vill, Random.Range(3,5) , 10, Random.Range(1, 5)));
+
+        return baseTasks;
     }
 }
