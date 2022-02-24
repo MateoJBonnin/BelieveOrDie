b98ee213f6da406900d77cf7dddb2e428b67ea9d
Mateo Javier Bonnin <mateojavierbonnin@gmail.com>
2022-02-23 23:37:43 -0300

    - Improved camera movement.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,53 +11,60 @@ public class CameraController : MonoBehaviour
     public float sensitivity;
     public float scrollSensitivity;
     public float dragSensitivity;
+    public float zoomInDistance;
 
     private Vector3 startPosition;
+    private Vector3 targetMovementOffset;
+    private Vector3 currentMovementOffset;
+    private Vector3 currentTargetDragPosition;
+    private Vector3 currentDragPosition;
+    private Vector3 currentDiffDragPosition;
+    private Vector3 startDragPosition;
+    private Vector3 maxDragPosition;
     Vector3 lastMousePosition;
-
+    private float currentScrollZoom;
 
     private void Start()
     {
         startPosition = transform.position;
+        this.startDragPosition = this.transform.position;
+        this.maxDragPosition = (this.transform.forward * this.zoomInDistance) + this.startDragPosition;
+        this.currentDragPosition = this.startDragPosition;
     }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.Mouse2))
         {
-            Vector3 movement = new Vector3(Input.mousePosition.x - lastMousePosition.x, 0, Input.mousePosition.y - lastMousePosition.y);
-
-            transform.position += -movement * Time.deltaTime * dragSensitivity;
+            targetMovementOffset -= new Vector3(Input.mousePosition.x - lastMousePosition.x, 0, Input.mousePosition.y - lastMousePosition.y) * dragSensitivity;
             lastMousePosition = Input.mousePosition;
         }
         else
         {
             lastMousePosition = Input.mousePosition;
-            Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), -Input.mouseScrollDelta.y, Input.GetAxisRaw("Vertical")).normalized;
+            this.targetMovementOffset += new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
-            if (Input.mousePosition.x > 0 && Input.mousePosition.x < Screen.width * 0.1f)
-            {
-                movement.x = -1;
-            }
-            else if (Input.mousePosition.x > Screen.width - Screen.width * 0.1f && Input.mousePosition.x < Screen.width)
-            {
-                movement.x = 1;
-            }
+            this.currentScrollZoom += Input.mouseScrollDelta.y * this.scrollSensitivity * Time.deltaTime;
+            this.currentScrollZoom = Mathf.Clamp01(this.currentScrollZoom);
 
-            if (Input.mousePosition.y > 0 && Input.mousePosition.y < Screen.height * 0.1f)
+            if ((Input.mousePosition.x > 0 && Input.mousePosition.x < Screen.width * 0.1f) && targetMovementOffset.x >= startPosition.x - xSize)
             {
-                movement.z = -1;
+                this.targetMovementOffset.x -= 1;
             }
-            else if (Input.mousePosition.y > Screen.height - Screen.height * 0.1f && Input.mousePosition.y < Screen.height)
+            else if ((Input.mousePosition.x > Screen.width - Screen.width * 0.1f && Input.mousePosition.x < Screen.width) && targetMovementOffset.x < startPosition.x + xSize)
             {
-                movement.z = 1;
+                this.targetMovementOffset.x += 1;
             }
 
-
-            Vector3 toPosition = transform.position + new Vector3(movement.x * sensitivity, movement.y * scrollSensitivity, movement.z * sensitivity);
-            transform.position = Vector3.Lerp(transform.position, toPosition, Time.deltaTime);
+            if ((Input.mousePosition.y > 0 && Input.mousePosition.y < Screen.height * 0.1f) && targetMovementOffset.z >= startPosition.x - zSize)
+            {
+                this.targetMovementOffset.z -= 1;
+            }
+            else if ((Input.mousePosition.y > Screen.height - Screen.height * 0.1f && Input.mousePosition.y < Screen.height) && targetMovementOffset.z < startPosition.x + zSize)
+            {
+                this.targetMovementOffset.z += 1;
+            }
         }
-
 
         if (transform.position.x >= startPosition.x + xSize)
         {
@@ -85,8 +92,13 @@ public class CameraController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, startPosition.y - ySize, transform.position.z);
         }
-    }
 
+        this.currentTargetDragPosition = Vector3.Lerp(this.startDragPosition, this.maxDragPosition, this.currentScrollZoom);
+        this.currentDragPosition = Vector3.Lerp(this.currentDragPosition, this.currentTargetDragPosition, Time.deltaTime * 20f);
+        this.currentMovementOffset = Vector3.Lerp(this.currentMovementOffset, this.targetMovementOffset * sensitivity, Time.deltaTime * 15f);
+        Vector3 movementDiff = (currentDragPosition + this.currentMovementOffset) - this.transform.position;
+        transform.position = Vector3.Lerp(transform.position, transform.position + movementDiff, Time.deltaTime * 5f);
+    }
 
     private void OnDrawGizmos()
     {
